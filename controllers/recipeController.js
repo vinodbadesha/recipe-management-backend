ffaf9891fe7234ccc8f20e7ffea358e55eae36bf
Vinod Badesha <badeshavinod@gmail.com>
2025-03-08 16:58:26 +0530

    created get/update/delete APIs

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,5 +1,7 @@
 const Recipe = require("../models/Recipe")
 
+// 1.CREATING A RECIPE
+
 const createRecipe = async (request, response) => {
     try{
         const {title, ingredients, instructions, categories} = request.body
@@ -15,4 +17,63 @@ const createRecipe = async (request, response) => {
     }
 }
 
-module.exports = {createRecipe}
+// 2.GET ALL RECIPES
+
+const getRecipe = async (request, response) => {
+    try{
+        const recipe = await Recipe.find()
+        response.status(200).json(recipe)
+    }
+    catch(error){
+        console.log(error)
+        response.status(500).json({message: "Server error"})
+    }
+}
+
+// 3.GET RECIPE BY ID
+
+const getRecipeById = async(request, response) => {
+    try{
+        const recipe = await Recipe.findById(request.params.id)
+        if (!recipe){
+            return response.status(404).json("Recipe not found")
+        }
+        response.status(200).json(recipe)
+    }
+    catch(error){
+        response.status(500).json({message: "Server error"})
+    }
+}
+
+// 4.UPDATE THE RECIPE BY ID
+
+const updateRecipeById = async (request, response) => {
+    try{
+        const {title, ingredients, instructions, categories} = request.body
+        const recipe = await Recipe.findByIdAndUpdate(request.params.id, {title, ingredients, instructions, categories})
+        if (!recipe){
+            return response.status(404).json("Recipe not found")
+        }
+        response.status(200).json(recipe)
+    }
+    catch(error){
+        response.status(500).json("Server error")
+    }
+}
+
+// 5.DELETE RECIPE BY ID
+
+const deleteRecipe = async(request, response) => {
+    try{
+        const recipe = await Recipe.findByIdAndDelete(request.params.id)
+        if (!recipe){
+            return response.status(404).json("Recipe not found")
+        }
+        response.status(200).json("Recipe deleted")
+    }
+    catch(error){
+        response.status(500).json({message: "Server error"})
+    }
+}
+
+module.exports = {createRecipe, getRecipe, getRecipeById, deleteRecipe, updateRecipeById}
--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -4,5 +4,9 @@ const recipeController = require("../controllers/recipeController")
 const recipeModel = require("../models/Recipe")
 
 router.post("/add-recipe", recipeController.createRecipe)
+router.get("/get-all-recipes", recipeController.getRecipe)
+router.get("/get-recipe/:id", recipeController.getRecipeById)
+router.delete("/delete-recipe/:id", recipeController.deleteRecipe)
+router.put("/update-recipe/:id", recipeController.updateRecipeById)
 
 module.exports = router
