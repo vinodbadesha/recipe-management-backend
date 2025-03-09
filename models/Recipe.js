a9136fbdb6981468b038046d7109e52ab3af31ba
Vinod Badesha <badeshavinod@gmail.com>
2025-03-09 15:21:38 +0530

    updated schema 2

--- models/Recipe.js
+++ models/Recipe.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose")
 const RecipeSchema = new mongoose.Schema({
     title: {type: String, required: true},
     imageUrl: {type: String},
-    ingredients: {type: String, required: true},
-    instructions: {type: String, required: true},
+    ingredients: {type: [String], required: true},
+    instructions: {type: [String], required: true},
+    time: {type: String, required: true},
     categories: {type: String, required: true}
 })
 
